3f3998fd2cfb34a99c84c6d34e782dca33b64e4f
Liviaalencr <Liviaalencr@gmail.com>
2025-04-01 19:34:14 +0000

    desafio-batalha-naval-nivel-novato-commit

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
+    
     char  linha [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
-    printf("       Batalha Naval \n");
-    
-    // Inicializando o tabuleiro com 0 (água)
+    printf("   #####  Batalha Naval  ##### \n");
+    printf("\n");
 
+    // Inicializando o tabuleiro com 0 (água)
     int Tabuleiro[10][10];
     for (int i = 0; i < 10; i++){
         for (int j = 0; j < 10; j++)
@@ -17,18 +18,20 @@ int main() {
     }
     
     // Cordenadas dos navios 
-    int linhaHorizontal = 2, colunaHorizontal = 4;
+    int linhaHorizontal = 8, colunaHorizontal = 2;
     int linhaVertical = 5, colunaVertical = 7;
 
 
     // Posicionamento do navio 1                                   // Navio 1
-    for (int j = 0; j < 3; j++){
+    for (int j = 0; j < 3; j++)
+    {
         Tabuleiro[linhaHorizontal][colunaHorizontal + j] = 1;
     }
     
     // Posicionamento do navio 2                                    // Navio 2
-    for (int i = 0; i < 3; i++){                                     // 3 é definindo a quantidade de casas
-    if (Tabuleiro[linhaVertical + i][colunaVertical] == 0) 
+    for (int i = 0; i < 3; i++)                                     // 3 é definindo a quantidade de casas
+    {
+        if (Tabuleiro[linhaVertical + i][colunaVertical] == 0) 
         {
             Tabuleiro[linhaVertical + i][colunaVertical] = 2;
         }
@@ -52,7 +55,6 @@ int main() {
             printf("%d  ", Tabuleiro[i][j]);  // zeros
         }
         
-        printf("\n");
         printf("\n");
     }
     
