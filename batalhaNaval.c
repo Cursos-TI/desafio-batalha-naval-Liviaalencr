5cf9782cca82d05922ceb612bfad0a87f099c3dc
Liviaalencr <Liviaalencr@gmail.com>
2025-04-05 23:10:16 +0000

    desafio-batalha-naval-nivel-mestre-commit

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,78 +1,103 @@
 #include <stdio.h>
+#include <string.h>
+#define TAMANHO 10
+#define NAVIO 3
+
+// Declaração dos protótipos das funções
+int inicializarTabuleiro(int tabuleiro[TAMANHO][TAMANHO]);
+int exibirTabuleiro(int tabuleiro[TAMANHO][TAMANHO]);
+int aplicarHabilidade(int tabuleiro[TAMANHO][TAMANHO], int habilidade[5][5], int origemX, int origemY);
+
+// Matriz Cone (5x5)
+int cone[5][5] = {
+    {0, 0, 1, 0, 0},
+    {0, 1, 1, 1, 0},
+    {1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0}
+};
+
+// Matriz Cruz (5x5)
+int cruz[5][5] = {
+    {0, 0, 1, 0, 0},
+    {0, 0, 1, 0, 0},
+    {1, 1, 1, 1, 1},
+    {0, 0, 1, 0, 0},
+    {0, 0, 1, 0, 0}
+};
+
+// Matriz Octaedro (5x5)
+int octaedro[5][5] = {
+    {0, 0, 1, 0, 0},
+    {0, 1, 1, 1, 0},
+    {1, 1, 1, 1, 1},
+    {0, 1, 1, 1, 0},
+    {0, 0, 1, 0, 0}
+};
 
 int main() {
-    
-    char  linha [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-
-    printf("   #####  Batalha Naval  ##### \n");
     printf("\n");
-
-    // Inicializando o tabuleiro com 0 (água)
-    int Tabuleiro[10][10];
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 10; j++)
-        {
-            Tabuleiro[i][j] = 0;
-        }
-        
-    }
+    int tabuleiro[TAMANHO][TAMANHO];
+    inicializarTabuleiro(tabuleiro);
     
-    // Cordenadas dos navios 1 e 2
-    int linhaHorizontal = 8, colunaHorizontal = 2;      // 8 = linha vertical 2 = linha vertical
-    int linhaVertical = 4, colunaVertical = 1;          // 4 = linha vertical 1 = linha vertical
+    printf("Tabuleiro inicial:\n");
+    exibirTabuleiro(tabuleiro);
+    
+    // Aplicação das habilidades
+    aplicarHabilidade(tabuleiro, cone, 4, 4);
+    printf("\nTabuleiro com saída de habilidade Cone:\n");
+    exibirTabuleiro(tabuleiro);
+    
+    inicializarTabuleiro(tabuleiro);
+    aplicarHabilidade(tabuleiro, cruz, 4, 4);
+    printf("\nTabuleiro com saída de habilidade Cruz:\n");
+    exibirTabuleiro(tabuleiro);
+    
+    inicializarTabuleiro(tabuleiro);
+    aplicarHabilidade(tabuleiro, octaedro, 4, 4);
+    printf("\nTabuleiro com saída de habilidade Octaedro:\n");
+    exibirTabuleiro(tabuleiro);
+    
+    return 0;
+}
 
-
-    // Posicionamento do navio 1                                   // Navio 1
-    for (int j = 0; j < 3; j++)
+// Função para inicializar o tabuleiro com água (0) e um navio (3) em uma posição fixa
+int inicializarTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
+    for (int i = 0; i < TAMANHO; i++) 
     {
-        Tabuleiro[linhaHorizontal][colunaHorizontal + j] = 1;
+        for (int j = 0; j < TAMANHO; j++) {
+            tabuleiro[i][j] = 0;
+        }
     }
-    
-    // Posicionamento do navio 2                                    // Navio 2
-    for (int i = 0; i < 3; i++)                                     // 3 é definindo a quantidade de casas
+    tabuleiro[4][4] = 1;                                // Saida do primeiro navio
+    return 0;
+}
+
+// Função para exibir o tabuleiro
+int exibirTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
+    for (int i = 0; i < TAMANHO; i++) 
     {
-        if (Tabuleiro[linhaVertical + i][colunaVertical] == 0) 
-        {
-            Tabuleiro[linhaVertical + i][colunaVertical] = 2;
-        }
-        
-    }
-
-    // Coordenadas dos navios diagonais 3 e 4                   
-    int linhaDiagonal1 = 1, colunaDiagonal1 = 1;
-    int linhaDiagonal2 = 6, colunaDiagonal2 = 8;
-
-    // Posicionamento do navio 3 (diagonal principal)               // Navio 3
-     for (int i = 0; i < 3; i++) {
-        if (Tabuleiro[linhaDiagonal1 + i][colunaDiagonal1 + i] == 0) {
-            Tabuleiro[linhaDiagonal1 + i][colunaDiagonal1 + i] = 3;
-        }
-    }
-
-    // Posicionamento do navio 4 (diagonal secundária)              // Navio 4
-    for (int i = 0; i < 3; i++) {
-        if (Tabuleiro[linhaDiagonal2 - i][colunaDiagonal2 - i] == 0) {
-            Tabuleiro[linhaDiagonal2 - i][colunaDiagonal2 - i] = 3;
-        }
-    }
-
-    
-    // Preenchimento das letras nas linhas
-    // Exibição do tabuleiro
-    printf("  ");                       // Espaço para alinhamento
-    for (int j = 0; j < 10; j++) {
-        printf("%2d ", j);
-    }
-    printf("\n");
-
-    for (int i = 0; i < 10; i++) {
-        printf("%c ", linha[i]);        // Letras A-J para referência
-        for (int j = 0; j < 10; j++) {
-            printf("%d  ", Tabuleiro[i][j]);
+        for (int j = 0; j < TAMANHO; j++) {
+            printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
-
     return 0;
-    
 }
+
+// Função para aplicar uma matriz de habilidade ao tabuleiro
+int aplicarHabilidade(int tabuleiro[TAMANHO][TAMANHO], int habilidade[5][5], int origemX, int origemY) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) 
+        {
+            int x = origemX + i - 2;
+            int y = origemY + j - 2;
+            if (x >= 0 && x < TAMANHO && y >= 0 && y < TAMANHO && habilidade[i][j] == 1) {
+                tabuleiro[x][y] = 7;
+            }
+        }
+    }
+    return 0;
+}
+
+;
